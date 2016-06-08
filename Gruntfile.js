0db6c53ec042ddbdaf78e18112a110e8d45130e4
wizard <yongjiandefashi@163.com>
2016-06-08 14:34:12 +0800

    Gruntfile 配置终于成功了

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,19 +2,28 @@ module.exports = function(grunt) {
 	// Do grunt-related things in here
 	// Project configuration.
 	grunt.initConfig({
-		concat: {
-			// 这里是concat任务的配置信息。
-		},
+		// concat: {
+		// 	// 这里是concat任务的配置信息。
+		// },
+		pkg: grunt.file.readJSON('package.json'),
 		uglify: {
 			// 这里是uglify任务的配置信息
-			src: ['js/*.js', 'static/**/*.js'],
-			dest: ['js/all.js']
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			build: {
+				src: ['src/**/**.js'],
+				dest: 'dist/js/all.js'
+			}
 		},
 		// 任意数据。
 		my_property: 'whatever',
-		my_src_files: ['js/*.js', 'static/**/*.js'],
-		pkg: grunt.file.readJSON('package.json')
+		my_src_files: ['src/**/*.js']
+
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// 默认被执行的任务列表。
+	grunt.registerTask('default', ['uglify']);
 };
