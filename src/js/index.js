8e7d5dc2aed0de5a793ce6f03e1e479ce9d445dc
wizard <yongjiandefashi@163.com>
2016-06-12 14:47:43 +0800

    给项目添加seajs

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
-new Vue({
-	el: '#app',
-	data: {
-		message: 'Hello Vue.js!'
-	}
-})
+define(function(require, exports, module) {
+	require('vue');
+	module.exports = {};
+	new Vue({
+		el: '#app',
+		data: {
+			message: 'Hello Vue.js!'
+		}
+	});
+});
